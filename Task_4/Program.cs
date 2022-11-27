4df974a88a1df2ec29b084d0002aeab14fa47382
Даниил <geimer_cok@mail.ru>
2022-11-27 18:31:00 +0300

    четвёртая задача

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = a; 
+while (count <= a)
+{
+count-- ;
+int b = a * ( -1 ); 
+Console.WriteLine(b);
+}
+while (count >= a)
+{
+count++; 
+Console.WriteLine(a);
+}
