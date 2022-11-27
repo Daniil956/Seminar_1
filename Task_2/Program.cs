5293739fd8bc4221b7a08faf13fe164d415449c1
Даниил <geimer_cok@mail.ru>
2022-11-27 18:25:35 +0300

    вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32 (Console.ReadLine());
+
+double sqeare =  b * b;
+if (a == sqeare)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является квадратом числа {b}");
+}
