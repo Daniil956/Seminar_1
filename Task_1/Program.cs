426bf8c26157d904d2a901f652c058ba8fc5a31f
Даниил <geimer_cok@mail.ru>
2022-11-27 18:21:27 +0300

    первая задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqaer = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqaer}");
